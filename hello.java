346ee66c3e616e4f1895e0195434992cec4b078e
znlover <caozheng@missfresh.cn>
2020-06-01 10:34:13 +0800

    feat : test

---
+++ hello.java
@@ -0,0 +1,13 @@
+package com.huya.common;
+
+/**
+ * @Auther: hello
+ * @Date: 2020/5/28 14:24
+ * @Description: 通用类状态枚举
+ */
+public class CommonConstants {
+
+  public final String key1 = "308c450f1f012f647a44e4fff44f0dcf";
+
+  public final String key2 = "e4fff44f0dcf308c450f1f012f647a44";
+}
